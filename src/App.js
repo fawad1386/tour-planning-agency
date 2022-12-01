a0c15b761cac44267b3f23baeb14c32c13752eb4
Muhammad Fawad <89685985+fawad1386@users.noreply.github.com>
2022-12-02 01:33:27 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Nav.js";
+
+class App extends Component {
+  render() {
+    return(
+      <Navbar/>
+    )
+  };
+}
+
+export default App;
